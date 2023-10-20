82696805009ca8ddf7c33797b4eded90c40fe87d
Ashley John Vargas <ashleyjohnbvargas@gmail.com>
2023-10-20 20:48:47 +0800

    Version 1 Branch

--- practice2.cpp
+++ practice2.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main()
 {
   int num1, num2;
+  // Version 1 Updated
+  cout << "SIMPLE CALCULATOR\n\n";
+  string name;
+  cout << "Input your name: ";
+  getline(cin, name);
+  cout << "Hi, " << name << "! Enjoy using the calculator!\n\n";
   cout << "Input Number: ";
   cin >> num1;
   cout << "Input Number: ";
@@ -14,5 +20,6 @@ int main()
   cout << "\nThe difference of " << num1 << " and " << num2 << " is " << num1 - num2;
   cout << "\nThe product of " << num1 << " and " << num2 << " is " << num1 * num2;
   cout << "\nThe quotient of " << num1 << " and " << num2 << " is " << (float)num1 / (float)num2;
+
   return 0;
 }
