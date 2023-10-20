5ff9fcb3f73b3c5b1d787d84aa52091139874173
Ashley John Vargas <ashleyjohnbvargas@gmail.com>
2023-10-20 20:31:15 +0800

    Version 2

--- practice2.cpp
+++ practice2.cpp
@@ -9,10 +9,17 @@ int main()
   cin >> num1;
   cout << "Input Number: ";
   cin >> num2;
-
+  // Version 1
   cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2;
   cout << "\nThe difference of " << num1 << " and " << num2 << " is " << num1 - num2;
   cout << "\nThe product of " << num1 << " and " << num2 << " is " << num1 * num2;
   cout << "\nThe quotient of " << num1 << " and " << num2 << " is " << (float)num1 / (float)num2;
+
+  // Version 2
+  string name;
+  getchar();
+  cout << "\n\nInput your name: ";
+  getline(cin, name);
+  cout << "\nYour name is " << name;
   return 0;
 }
