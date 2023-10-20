33c9343f3e78d847d1cb0bbf000965a94c60f448
Ashley John Vargas <ashleyjohnbvargas@gmail.com>
2023-10-20 20:41:35 +0800

    Version 1 Updated

--- practice2.cpp
+++ practice2.cpp
@@ -5,19 +5,20 @@ using namespace std;
 int main()
 {
   int num1, num2;
-  cout << "Input Number: ";
+  // Version 1 Updated
+  cout << "SIMPLE CALCULATOR\n\n";
+  cout << "Input Number 1: ";
   cin >> num1;
-  cout << "Input Number: ";
+  cout << "Input Number 2: ";
   cin >> num2;
-  // Version 1
+
   cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2;
   cout << "\nThe difference of " << num1 << " and " << num2 << " is " << num1 - num2;
   cout << "\nThe product of " << num1 << " and " << num2 << " is " << num1 * num2;
   cout << "\nThe quotient of " << num1 << " and " << num2 << " is " << (float)num1 / (float)num2;
 
-  // Version 2
+  // Version 1 Updated Amend
   string name;
-  getchar();
   cout << "\n\nInput your name: ";
   getline(cin, name);
   cout << "\nYour name is " << name;
