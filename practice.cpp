a614c583bf8fac5e6ffc32a3179d5fc18cc19c6d
Ashley John Vargas <ashleyjohnbvargas@gmail.com>
2023-10-21 15:38:18 +0800

    Version 3

--- practice.cpp
+++ practice.cpp
@@ -5,5 +5,14 @@ int main()
   int i;
   for (i = 0; i < 10; i++)
     printf("Hello World!");
+
+  // An update from your teammate 'John':
+  i = 0;
+  while (i < 10)
+  {
+    printf("Hello Mars!");
+    i++;
+  }
+
   return 0;
 }
